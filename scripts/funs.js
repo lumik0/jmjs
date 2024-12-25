b5206c565275b174cfac2ca377a6e14edb264b9d
Люмик <75566052+lumik0@users.noreply.github.com>
2024-12-25 10:27:48 +0300

    1.1b

--- scripts/funs.js
+++ scripts/funs.js
@@ -4,14 +4,18 @@
             if(!((cHandler === null)||(cHandler === cMain))){
                 return error(null, `Нельзя определять функции в ивенте`);
             }
+            let options = {
+                hidden: false
+            }
             if(typeof value != 'function'){
-                return error(null, `Функция ${property} должна быть функцией`);
+                if(typeof value != 'object' || Array.isArray(value)) throw (`Функция ${property} должна быть функцией`);
+                options = value; value = options.func || options.fun || options.function || options.callback;
             }
             let old = cHandler, c;
             if(!target[property]){
                 c = addHandler({
                     type: 'function',
-                    is_hidden: false,
+                    is_hidden: options.hidden,
                     name: property,
                     operations: []
                 });
@@ -37,6 +41,13 @@
         }
     });
     function callFunction(name, ...args){
+        if(typeof name == 'function'){
+            for(let i in functions){
+                if(functions[i] == name) {
+                    name = i; break;
+                }
+            }
+        }
         args.forEach((arg, i) => {
             game[`JMJS_funs:${name}_${i}`] = arg;
         });
@@ -86,6 +97,14 @@
         }
     });
     function startProcess(name, local_variables_mode, target_mode, ...args){
+        if(typeof name == 'function'){
+            for(let i in functions){
+                if(processes[i] == name) {
+                    name = i; break;
+                }
+            }
+        }
+        
         for(let i=0;i<args.length;i++){
             game[`JMJS_proc:${name}_${i}`] = args[i];
         }
